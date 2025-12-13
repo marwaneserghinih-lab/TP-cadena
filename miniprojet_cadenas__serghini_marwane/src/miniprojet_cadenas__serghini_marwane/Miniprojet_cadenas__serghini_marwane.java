804661268b82eb2e056c35239a97ddb5f6315b53
marwane serghini <marwane.serghini@epfedu.fr>
2025-12-13 23:55:58 +0100

    final interface brouillon

---
+++ miniprojet_cadenas__serghini_marwane/src/miniprojet_cadenas__serghini_marwane/Miniprojet_cadenas__serghini_marwane.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package miniprojet_cadenas__serghini_marwane;
+
+public class Miniprojet_cadenas__serghini_marwane {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
